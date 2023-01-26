4300a91e76a761ee06be058e693638ef2cfbf914
Rajeev K Rao <rajeevraok@gmail.com>
2023-01-26 15:01:16 +0530

    strict cors origins

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ mongo.connect();
 
 const app = express();
 
-app.use(cors()); 
-//Uncomment to allow cors for all routes
+app.use(cors({credentials: true,origin:
+    [
+        'http://127.0.0.1:5173',
+        'http://localhost:5173'
+    ]
+}))
 
 
 app.use(express.urlencoded({ extended: true }));
