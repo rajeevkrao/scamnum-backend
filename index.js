100514521d81b9640ca4e3bd7d457342a1ff07e3
Rajeev K Rao <rajeevraok@gmail.com>
2023-01-26 15:03:15 +0530

    adding vercel domain to cors

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const app = express();
 app.use(cors({credentials: true,origin:
     [
         'http://127.0.0.1:5173',
-        'http://localhost:5173'
+        'http://localhost:5173',
+        'https://scamnum.vercel.app/'
     ]
 }))
 
